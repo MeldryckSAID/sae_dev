1e4ec267a4a16b2fd33f58ee222abada8b8a0c73
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-05-30 08:41:40 +0200

    first commit

---
+++ src/components/head.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <main>
+    <h1 class="text-2xl"> test head sur acceu</h1>
+  </main>
+
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/views/AccueilView.vue
@@ -0,0 +1,19 @@
+<template>
+
+ <header>
+  <div class="text-rose-700 text-center text-8xl bg-blue-600">
+    test acceuiel sur app
+  </div>
+
+  
+
+  
+ </header>
+
+</template>
+
+<script>
+import head from '../components/head.vue';
+export default {
+  components: { head },};
+</script>
