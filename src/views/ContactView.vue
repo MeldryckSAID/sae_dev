aaaab355efcd07fb93b2a1ec863295e31f1333fa
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-05-31 20:44:08 +0200

    corectif contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -10,7 +10,37 @@
         Des Questions ? <br />
         Nous contacter ici.
       </h2>
-      <BouttonView2>+</BouttonView2>
+      <div class="flex flex-col items-center">
+
+        <form action="flex flex-col">
+
+          <div class="p-5">
+            <label for="fname">Nom  </label>
+            <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom" />
+          </div>
+
+          <div class="p-5">
+            <label for="fname">  prénom</label>
+            <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom" />
+          </div>
+
+          <div class="p-5">
+            <label for="sujet">Sujet</label>
+            <input type="text" id="sujet" name="sujet" placeholder="L'objet de votre message" />
+          </div>
+          
+          <div class="p-5">
+            <label for="emailAddress">Email</label>
+            <input id="emailAddress" type="email" name="email" placeholder="Votre email" />
+          </div>
+
+          <div class="p-5 flex">
+            <label  for="subject">Message</label>
+            <textarea id="subject" name="subject" placeholder="Votre message" style="height: 200px"></textarea>
+          </div>
+
+        </form>
+      </div>
     </main>
 
     <footer>
