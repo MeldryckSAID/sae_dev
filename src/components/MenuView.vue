0ab28ffa42eea088d80c26bc5036075c23846b5d
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-01 11:18:19 +0200

    aniation

--- src/components/MenuView.vue
+++ src/components/MenuView.vue
@@ -309,145 +309,144 @@
     </div>
 
     <!-- Menu mobile -->
-    <div
-      class="top- fixed left-0 z-50 h-screen w-screen animate-fadeInLeftBig bg-gradient-to-l from-cream to-warm transition duration-1000 ease-out"
-      :class="{ hidden: menuVisible }"
+    <Transition
+      enter-active-class="animate-fadeInLeftBig  duration-1000 ease-out"
+      leave-active-class="animate-fadeOutLeftBig  duration-[3s] ease-in"
     >
-      <div class="fixed top-0 text-black ipad_pro:hidden">
-        <div class="flex flex-col items-center border-t-4 border-black bg-cream">
-          <HomeIcon class="w-9" />
-          <RouterLink class="block py-2 px-4 hover:text-white" to="/">Home</RouterLink>
-        </div>
-        <div class="flex flex-col items-center border-t-4 border-black bg-cream">
-          <ClockIcon class="w-9" />
-          <RouterLink class="block py-2 px-4 hover:text-white" to="/Programation">Programme</RouterLink>
-        </div>
-
-        <div class="flex flex-col items-center border-t-4 border-black bg-cream">
-          <UsersIcon class="w-9" />
-          <RouterLink class="block py-2 px-4 hover:text-white" to="/Artistes">Inviter</RouterLink>
-        </div>
-
-        <div class="flex flex-col items-center border-t-4 border-black bg-cream">
-          <svg
-            width="101"
-            height="101"
-            viewBox="0 0 101 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-[51px] w-[51px]"
-            preserveAspectRatio="none"
-          >
-            <circle cx="50.5" cy="50.5" r="50.5" fill="#020202"></circle>
-            <path
-              d="M63.0445 53.1566L64.8099 49.9293L71.9626 36.8502H78.3918L71.0905 49.9293L69.3124 53.1163L59.885 70.002L52.4607 72.5058L63.0445 53.1566Z"
-              fill="#626262"
-            ></path>
-            <path d="M78.084 49.9292L73.7554 53.1396V53.1565H69.3844L69.3123 53.1162L71.0904 49.9292H78.084Z" fill="white"></path>
-            <path
-              d="M39.9458 36.8502H42.5748H46.4641H71.9624L64.8096 49.9293H58.181L64.1604 39.4325H22.5955L22.6018 36.8502H36.5148H39.9458Z"
-              fill="#626262"
-            ></path>
-            <path
-              d="M64.8097 49.9293L63.0444 53.1566H57.4597H50.0396L55.3123 42.5389V42.4243H59.1784L54.9877 49.9293H58.1811H64.8097Z"
-              fill="white"
-            ></path>
-            <path
-              d="M64.1605 39.4325L58.1811 49.9293H54.9877L59.1784 42.4243H55.3124H46.5661H42.8338H39.9459H36.5149H22.585L22.5956 39.4325H64.1605Z"
-              fill="#626262"
-            ></path>
-            <path d="M63.0442 53.1566L52.4605 72.5058V72.5079L46.4641 74.53L57.4595 53.1566H63.0442Z" fill="#626262"></path>
-            <path
-              d="M46.566 42.4243V42.5389L46.5554 60.2457C46.5554 61.4446 45.3417 62.4121 43.8458 62.4121H29.5509L27.9192 59.9741H38.4287C40.9155 59.9741 42.927 58.788 42.927 57.3218L42.8337 42.5368V42.4243H46.566Z"
-              fill="white"
-            ></path>
-            <path
-              d="M46.4622 36.8248V36.8502H42.575V32.5641C42.575 30.3383 40.5508 28.5326 38.0491 28.5326H22.6296V26.6229V26.5232L38.6793 26.6166C42.9782 26.6166 46.458 28.5623 46.458 30.9663L46.4622 36.8248Z"
-              fill="white"
-            ></path>
-            <path
-              d="M39.9457 53.1566V42.4243H42.8335V42.5368L42.9269 57.3218C42.9269 58.788 40.9153 59.9741 38.4285 59.9741H27.919L25.689 56.2057H25.7081H37.7856C38.9802 56.1951 39.9457 54.8371 39.9457 53.1566Z"
-              fill="white"
-            ></path>
-            <path
-              d="M42.5749 36.7377V36.8544H39.9459V33.5019C39.9459 31.6813 38.6982 30.2066 37.1578 30.2066H22.6252V28.5367H38.0447C40.5464 28.5367 42.5706 30.3382 42.5706 32.5683L42.5749 36.7377Z"
-              fill="white"
-            ></path>
-            <path
-              d="M39.9459 42.5368V53.146C39.9459 54.8265 38.9805 56.1845 37.7859 56.1845H25.7083H25.6892L23.6289 52.7026H34.1384C35.4518 52.7026 36.5149 51.8856 36.5149 50.8778V42.4243H39.9459V42.5368Z"
-              fill="white"
-            ></path>
-            <path
-              d="M36.5148 32.7785H22.6167V30.2068H37.1492C38.6897 30.2068 39.9374 31.6815 39.9374 33.502V36.8503H36.5148V32.7785Z"
-              fill="white"
-            ></path>
-            <path d="M36.015 33.2783V36.3501H23.1039L23.1151 33.2783H36.015Z" fill="white" stroke="black"></path>
-            <path
-              d="M22.5847 42.4243L22.5953 39.4325L22.6017 36.8502L22.6165 32.7783L22.625 30.2067L22.6293 28.5368L22.6356 26.6313"
-              stroke="#1D1D1B"
-              stroke-miterlimit="10"
-            ></path>
-            <path d="M22.5847 30.2065H22.625H37.1576" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M22.5847 42.4242H36.5147H39.9457H42.8335H46.5658H55.3121H59.1781" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M22.5847 32.7783H22.6165H36.5147H36.6186" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M36.5149 32.7783V36.8501" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M39.9458 33.5018V36.8501V36.9583" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path
-              d="M22.5847 36.8502H22.6017H36.5147H39.9457H42.5747H46.464H71.9623H78.3915"
-              stroke="#1D1D1B"
-              stroke-miterlimit="10"
-            ></path>
-            <path d="M22.4915 39.4325H22.5954H64.1603" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M22.5276 26.5315H22.6358L38.6855 26.6249" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M46.4641 30.9747V36.8247" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M39.9458 42.5366V53.1565V53.7145" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M42.834 42.5366L42.9273 57.3217" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M36.5149 42.5388V50.8989" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M23.6289 52.7238H34.1384" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M25.708 56.1951H37.7856" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M27.9194 59.974H38.4289" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M36.5149 50.8989C36.5149 51.9068 35.454 52.7237 34.1384 52.7237" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M42.927 57.3196C42.927 58.7858 40.9155 59.9719 38.4287 59.9719" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M39.9457 53.1566C39.9457 54.8371 38.9802 56.1951 37.7856 56.1951" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M23.6289 52.7238L25.6892 56.2057L27.9193 59.9741" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M46.566 42.5388L46.5554 60.2456" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M46.5555 60.2456C46.5555 61.4445 45.3419 62.412 43.8459 62.412" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M43.8458 62.4121H29.5509H29.4724" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M27.9194 59.974L29.5511 62.412" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M22.5847 28.5369H22.6293H38.0488" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M42.575 32.5641V36.7335" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M39.9458 33.5018C39.9458 31.6812 38.6982 30.2065 37.1577 30.2065" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M42.575 32.5641C42.575 30.3383 40.5507 28.5326 38.0491 28.5326" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M46.4618 30.9747C46.4618 28.5706 42.982 26.6249 38.6831 26.6249" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M71.9626 36.8502L64.8099 49.9293L63.0445 53.1566L52.4607 72.5058" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M46.4641 74.5301L52.4605 72.508V72.5059L59.8848 70.0021" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M78.3915 36.8502L71.0902 49.9293L69.3121 53.1163L59.8848 70.002" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M59.1782 42.4242L54.9875 49.9292" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M55.3123 42.5388L50.0396 53.1565" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M50.0396 53.1566H57.4597H63.0444H63.2502" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M54.9875 49.9293H58.1809H64.8096" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M71.0903 49.9293H78.0839" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M69.3845 53.1566H73.7555H73.8362" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M78.0837 49.9293L73.7551 53.1397" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M64.1605 39.4325L58.1812 49.9293" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-            <path d="M46.4641 74.53L57.4595 53.1566" stroke="#1D1D1B" stroke-miterlimit="10"></path>
-          </svg>
-          <RouterLink class="block bg-cream py-2 px-4 hover:text-white" to="/Festival">Festival</RouterLink>
-        </div>
-
-        <div class="flex flex-col items-center border-t-4 border-black bg-cream">
-          <SparklesIcon class="w-9" />
-          <RouterLink class="block bg-cream py-2 px-4 hover:text-white" to="/Concert">Concert </RouterLink>
+      <div class="top- fixed left-0 z-50 h-screen w-screen bg-gradient-to-l from-cream to-warm" v-if="menuVisible">
+        <div class="top-0 w-max text-black ipad_pro:hidden">
+          <div class="flex flex-col items-center border-t-4 border-black bg-cream">
+            <HomeIcon class="w-9" />
+            <RouterLink class="block py-2 px-4 hover:text-white" to="/">Home</RouterLink>
+          </div>
+          <div class="flex flex-col items-center border-t-4 border-black bg-cream">
+            <ClockIcon class="w-9" />
+            <RouterLink class="block py-2 px-4 hover:text-white" to="/Programation">Programme</RouterLink>
+          </div>
+          <div class="flex flex-col items-center border-t-4 border-black bg-cream">
+            <UsersIcon class="w-9" />
+            <RouterLink class="block py-2 px-4 hover:text-white" to="/Artistes">Inviter</RouterLink>
+          </div>
+          <div class="flex flex-col items-center border-t-4 border-black bg-cream">
+            <svg
+              width="101"
+              height="101"
+              viewBox="0 0 101 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-[51px] w-[51px]"
+              preserveAspectRatio="none"
+            >
+              <circle cx="50.5" cy="50.5" r="50.5" fill="#020202"></circle>
+              <path
+                d="M63.0445 53.1566L64.8099 49.9293L71.9626 36.8502H78.3918L71.0905 49.9293L69.3124 53.1163L59.885 70.002L52.4607 72.5058L63.0445 53.1566Z"
+                fill="#626262"
+              ></path>
+              <path d="M78.084 49.9292L73.7554 53.1396V53.1565H69.3844L69.3123 53.1162L71.0904 49.9292H78.084Z" fill="white"></path>
+              <path
+                d="M39.9458 36.8502H42.5748H46.4641H71.9624L64.8096 49.9293H58.181L64.1604 39.4325H22.5955L22.6018 36.8502H36.5148H39.9458Z"
+                fill="#626262"
+              ></path>
+              <path
+                d="M64.8097 49.9293L63.0444 53.1566H57.4597H50.0396L55.3123 42.5389V42.4243H59.1784L54.9877 49.9293H58.1811H64.8097Z"
+                fill="white"
+              ></path>
+              <path
+                d="M64.1605 39.4325L58.1811 49.9293H54.9877L59.1784 42.4243H55.3124H46.5661H42.8338H39.9459H36.5149H22.585L22.5956 39.4325H64.1605Z"
+                fill="#626262"
+              ></path>
+              <path d="M63.0442 53.1566L52.4605 72.5058V72.5079L46.4641 74.53L57.4595 53.1566H63.0442Z" fill="#626262"></path>
+              <path
+                d="M46.566 42.4243V42.5389L46.5554 60.2457C46.5554 61.4446 45.3417 62.4121 43.8458 62.4121H29.5509L27.9192 59.9741H38.4287C40.9155 59.9741 42.927 58.788 42.927 57.3218L42.8337 42.5368V42.4243H46.566Z"
+                fill="white"
+              ></path>
+              <path
+                d="M46.4622 36.8248V36.8502H42.575V32.5641C42.575 30.3383 40.5508 28.5326 38.0491 28.5326H22.6296V26.6229V26.5232L38.6793 26.6166C42.9782 26.6166 46.458 28.5623 46.458 30.9663L46.4622 36.8248Z"
+                fill="white"
+              ></path>
+              <path
+                d="M39.9457 53.1566V42.4243H42.8335V42.5368L42.9269 57.3218C42.9269 58.788 40.9153 59.9741 38.4285 59.9741H27.919L25.689 56.2057H25.7081H37.7856C38.9802 56.1951 39.9457 54.8371 39.9457 53.1566Z"
+                fill="white"
+              ></path>
+              <path
+                d="M42.5749 36.7377V36.8544H39.9459V33.5019C39.9459 31.6813 38.6982 30.2066 37.1578 30.2066H22.6252V28.5367H38.0447C40.5464 28.5367 42.5706 30.3382 42.5706 32.5683L42.5749 36.7377Z"
+                fill="white"
+              ></path>
+              <path
+                d="M39.9459 42.5368V53.146C39.9459 54.8265 38.9805 56.1845 37.7859 56.1845H25.7083H25.6892L23.6289 52.7026H34.1384C35.4518 52.7026 36.5149 51.8856 36.5149 50.8778V42.4243H39.9459V42.5368Z"
+                fill="white"
+              ></path>
+              <path
+                d="M36.5148 32.7785H22.6167V30.2068H37.1492C38.6897 30.2068 39.9374 31.6815 39.9374 33.502V36.8503H36.5148V32.7785Z"
+                fill="white"
+              ></path>
+              <path d="M36.015 33.2783V36.3501H23.1039L23.1151 33.2783H36.015Z" fill="white" stroke="black"></path>
+              <path
+                d="M22.5847 42.4243L22.5953 39.4325L22.6017 36.8502L22.6165 32.7783L22.625 30.2067L22.6293 28.5368L22.6356 26.6313"
+                stroke="#1D1D1B"
+                stroke-miterlimit="10"
+              ></path>
+              <path d="M22.5847 30.2065H22.625H37.1576" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M22.5847 42.4242H36.5147H39.9457H42.8335H46.5658H55.3121H59.1781" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M22.5847 32.7783H22.6165H36.5147H36.6186" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M36.5149 32.7783V36.8501" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M39.9458 33.5018V36.8501V36.9583" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path
+                d="M22.5847 36.8502H22.6017H36.5147H39.9457H42.5747H46.464H71.9623H78.3915"
+                stroke="#1D1D1B"
+                stroke-miterlimit="10"
+              ></path>
+              <path d="M22.4915 39.4325H22.5954H64.1603" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M22.5276 26.5315H22.6358L38.6855 26.6249" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M46.4641 30.9747V36.8247" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M39.9458 42.5366V53.1565V53.7145" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M42.834 42.5366L42.9273 57.3217" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M36.5149 42.5388V50.8989" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M23.6289 52.7238H34.1384" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M25.708 56.1951H37.7856" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M27.9194 59.974H38.4289" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M36.5149 50.8989C36.5149 51.9068 35.454 52.7237 34.1384 52.7237" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M42.927 57.3196C42.927 58.7858 40.9155 59.9719 38.4287 59.9719" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M39.9457 53.1566C39.9457 54.8371 38.9802 56.1951 37.7856 56.1951" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M23.6289 52.7238L25.6892 56.2057L27.9193 59.9741" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M46.566 42.5388L46.5554 60.2456" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M46.5555 60.2456C46.5555 61.4445 45.3419 62.412 43.8459 62.412" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M43.8458 62.4121H29.5509H29.4724" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M27.9194 59.974L29.5511 62.412" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M22.5847 28.5369H22.6293H38.0488" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M42.575 32.5641V36.7335" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M39.9458 33.5018C39.9458 31.6812 38.6982 30.2065 37.1577 30.2065" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M42.575 32.5641C42.575 30.3383 40.5507 28.5326 38.0491 28.5326" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M46.4618 30.9747C46.4618 28.5706 42.982 26.6249 38.6831 26.6249" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M71.9626 36.8502L64.8099 49.9293L63.0445 53.1566L52.4607 72.5058" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M46.4641 74.5301L52.4605 72.508V72.5059L59.8848 70.0021" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M78.3915 36.8502L71.0902 49.9293L69.3121 53.1163L59.8848 70.002" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M59.1782 42.4242L54.9875 49.9292" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M55.3123 42.5388L50.0396 53.1565" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M50.0396 53.1566H57.4597H63.0444H63.2502" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M54.9875 49.9293H58.1809H64.8096" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M71.0903 49.9293H78.0839" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M69.3845 53.1566H73.7555H73.8362" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M78.0837 49.9293L73.7551 53.1397" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M64.1605 39.4325L58.1812 49.9293" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+              <path d="M46.4641 74.53L57.4595 53.1566" stroke="#1D1D1B" stroke-miterlimit="10"></path>
+            </svg>
+            <RouterLink class="block bg-cream py-2 px-4 hover:text-white" to="/Festival">Festival</RouterLink>
+          </div>
+          <div class="flex flex-col items-center border-t-4 border-black bg-cream">
+            <SparklesIcon class="w-9" />
+            <RouterLink class="block bg-cream py-2 px-4 hover:text-white" to="/Concert">Concert </RouterLink>
+          </div>
         </div>
       </div>
-    </div>
+    </Transition>
   </nav>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const menuVisible = ref(true);
+const menuVisible = ref(false);
 </script>
 
 <script>
