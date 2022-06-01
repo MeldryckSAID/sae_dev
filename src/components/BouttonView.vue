bc32c7ec0dd917e98f228244bc5fdbc388f25eee
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-01 09:10:52 +0200

    contact

--- src/components/BouttonView.vue
+++ src/components/BouttonView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex justify-end p-2">
-    <div class="relative inline-block items-center gap-2 rounded-3xl bg-gris p-7 py-2.5 text-center font-inter font-bold text-yellow-400">
+    <div
+      class="relative inline-block items-center gap-2 rounded-3xl bg-gris p-7 py-2.5 text-center font-inter font-bold text-yellow-400 active:bg-green-900 active:text-white active:shadow-lg"
+    >
       <p><slot /></p>
     </div>
   </div>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -10,7 +10,8 @@
         Des Questions ? <br />
         Nous contacter ici.
       </h2>
-      <div class="flex flex-col items-center">
+      <!-- <div class="flex flex-col items-center">
+        
 
         <form action="flex flex-col">
 
@@ -40,6 +41,77 @@
           </div>
 
         </form>
+      </div> -->
+
+      <div class="mt-5 flex justify-center p-5">
+        <form class="w-full max-w-sm">
+          <div class="mb-6 md:flex md:items-center">
+            <div class="md:w-1/3">
+              <label class="text-Black mb-1 block pr-4 font-bold md:mb-0 md:text-right" for="inline-full-name"> Nom </label>
+            </div>
+            <div class="md:w-2/3">
+              <input
+                class="w-full appearance-none rounded border-2 border-red-600 bg-gray-200 py-2 px-4 leading-tight text-black placeholder:text-black focus:border-green-500 focus:bg-white focus:outline-none"
+                id="inline-full-name"
+                type="text"
+                placeholder=" Votre Nom"
+              />
+            </div>
+          </div>
+          <div class="mb-6 md:flex md:items-center">
+            <div class="md:w-1/3">
+              <label class="text-Black mb-1 block pr-4 font-bold md:mb-0 md:text-right" for="inline-full-name"> Prénom </label>
+            </div>
+            <div class="md:w-2/3">
+              <input
+                class="w-full appearance-none rounded border-2 border-red-600 bg-gray-200 py-2 px-4 leading-tight text-black placeholder:text-black focus:border-green-500 focus:bg-white focus:outline-none"
+                id="inline-full-name"
+                type="text"
+                placeholder="Votre Prénom"
+              />
+            </div>
+          </div>
+
+          <div class="mb-6 md:flex md:items-center">
+            <div class="md:w-1/3">
+              <label class="text-Black mb-1 block pr-4 font-bold md:mb-0 md:text-right" for="inline-full-name"> Mail </label>
+            </div>
+            <div class="md:w-2/3">
+              <input
+                class="w-full appearance-none rounded border-2 border-red-600 bg-gray-200 py-2 px-4 leading-tight text-black placeholder:text-black focus:border-green-500 focus:bg-white focus:outline-none"
+                id="inline-full-name"
+                type="text"
+                placeholder=" Votre Mail"
+              />
+            </div>
+          </div>
+
+          <div class="flex items-start rounded-md">
+            <div class="md:w-1/3">
+              <label class="text-Black mb-1 block pr-4 font-bold md:mb-0 md:text-right" for="inline-full-name"> Votre message </label>
+            </div>
+            <div class="md:w-3/6">
+              <textarea
+                name="message"
+                class="ml-2 block w-full appearance-none rounded-md border-2 border-red-600 bg-gray-200 py-5 px-8 font-inter text-lg leading-tight text-black placeholder:text-black focus:border-green-500 focus:bg-white focus:outline-none"
+                type="text"
+                placeholder=" Dites nous tous  "
+                rows="5"
+                required
+              ></textarea>
+            </div>
+          </div>
+
+          <div class="mb-6 md:flex md:items-center">
+            <div class="md:w-1/3"></div>
+            <label class="block font-bold text-black md:w-2/3">
+              <input class="mr-2 leading-tight" type="checkbox" />
+              <span class="text-sm"> Send me your newsletter! </span>
+            </label>
+          </div>
+
+          <BouttonView>Envoyer</BouttonView>
+        </form>
       </div>
     </main>
 
@@ -52,9 +124,9 @@
 <script>
 import HeaderView from "../components/HeaderView.vue";
 import FooterView from "../components/FooterView.vue";
-import BouttonView2 from "../components/BouttonView2.vue";
+import BouttonView from "../components/BouttonView.vue";
 
 export default {
-  components: { HeaderView, FooterView, BouttonView2 },
+  components: { HeaderView, FooterView, BouttonView },
 };
 </script>
