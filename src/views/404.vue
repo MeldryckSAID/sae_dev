6a517dd6c8cfeaf47e8f57af4f5b547c58cf36b0
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-01 19:52:18 +0200

    base 404

---
+++ src/views/404.vue
@@ -0,0 +1,25 @@
+<template>
+    
+    <header>
+        <HeaderView></HeaderView>
+    </header>
+
+    <main class="bg-gris">
+        <div>
+            
+        </div>
+
+    </main>
+
+    <footer>
+        <FooterView></FooterView>
+    </footer>
+</template>
+
+<script>
+import HeaderView from "../components/HeaderView.vue";
+import FooterView from "../components/FooterView.vue";
+export default {
+  components: { HeaderView, FooterView,  },
+};
+</script>
