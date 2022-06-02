f13c0d8a261ccb02b17ff4a4d98352185b0676be
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-06-02 09:20:26 +0200

    correction

--- src/views/ConcertView.vue
+++ src/views/ConcertView.vue
@@ -13,13 +13,13 @@
         Le show sera exclusive vos artistes dancehall préférez assurons le spectacle afin de vous ambiancez 
       </p>
    
-        <h3 class="m-3 text-green-800 text-center font-fredoka text-3xl">00h30</h3>
+        <h3 class="m-3 text-green-800 text-center font-fredoka text-3xl">Jour 3</h3>
         <Card nom="Buju Banton" image="  /artiste/buju.png"></Card>
 
-        <h3 class="m-3 text-yellow-600 text-center font-fredoka text-3xl">00h30</h3>
+        <h3 class="m-3 text-yellow-300 stroke-black stroke-2  text-center font-fredoka text-3xl">Jour 2</h3>
         <Card2 nom="Shenseea" image="  /artiste/sheensea.png"></Card2>
 
-        <h3 class="m-3 text-red-800 text-center font-fredoka text-3xl">00h30</h3>
+        <h3 class="m-3 text-red-600 text-center font-fredoka text-3xl">Jour 1</h3>
         <Card class="pb-5" nom="Kalash" image="  /artiste/kalash.png"></Card>
 
         
